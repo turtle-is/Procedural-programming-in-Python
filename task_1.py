dc1034c3186d21bd5223402334447367794c97d4
Jeremy <73174006+turtle-is@users.noreply.github.com>
2023-10-29 20:31:07 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Поиск индекса элемента в списке товаров
+
+def find_index(item_list, name):
+    for i, index in enumerate(item_list):
+        if index == name:
+            return i
+
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ task_2.py
@@ -0,0 +1,23 @@
+# Поиск общих участников
+
+def find_common_participants(group_one, group_two, delimiter=','):
+    list_group1 = group_one.split(delimiter)
+    list_group2 = group_two.split(delimiter)
+    intersection_ = set(list_group1).intersection(list_group2)
+    return sorted(intersection_)
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group, delimiter='|'))
+
+participants_first_group_ = "Иванов Петров Сидоров Абвгедейкин"
+participants_second_group_ = "Абвгедейкин Петров Сидоров Смирнов"
+
+print(find_common_participants(participants_first_group_, participants_second_group_, delimiter=' '))
+
+participants_first_group_ = "Иванов,Петров,Шелби,Сидоров,Абвгедейкин"
+participants_second_group_ = "Абвгедейкин,Петров,Сидоров,Шелби,Смирнов"
+
+print(find_common_participants(participants_first_group_, participants_second_group_))
